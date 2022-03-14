ca3fee4b0adfb7efbe6574fa3e09be53280a0616
MadhanM282 <madhanm282@gmail.com>
2022-03-14 10:58:22 +0530

    list

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import { Rentals } from "./Components/Rentals/Rentals";
 import { AddHouse } from "./Components/AddHouse/AddHouse";
+import { useState } from "react";
 
 function App() {
+  const [show,setShow] =useState(false);
   return (
     <div className="App">
       <AddHouse/>
-      
+
       <Rentals/>
-      <button className="toggleForm">
+      <button className="toggleForm" onClick = {()=>{
+        setShow(true);
+      }}>
+
+
         {/* Show text Add House or Show Rentals based on state */}Add House
       </button>
       {/* Show component based on state */}
